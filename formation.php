e7150446c1e6b25f7cc43897756a35bef0ac582b
ingenieurmamar <77579317+ingenieurmamar@users.noreply.github.com>
2021-01-21 18:19:16 +0100

    Add files via upload

---
+++ formation.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="grid.css">
+	<title>Accueil</title>
+</head>
+<body>
+	<?php 
+	include 'menu.php';
+	?>
+	<div class="wrapper">	
+	<div class="defile">
+		<img src="IMG/img12.jpg">		
+	</div>
+	<div class="pub">
+        <img src="IMG/img6.jpg">
+	</div>
+	<div class="contenu">
+		<h1>Nos Formations :</h1>
+		<p>Chez IIR Niger, nous vous offrons une formation de qualité pour vous permettre l'accès à l'emploi:
+		</p>
+		<p><ul><li>AGENT BUREAUTIQUE : </li></ul> L'apprenant sera initié aux outils bureautiques de Windows, Word et Excel + l'Internet sur une période complète de trois (3) mois.
+		</p>
+		<p>==>AGENT DE SAISIE : L'apprenant sera initié aux outils bureautiques de Windows, Word et Excel + l'Internet sur une période complète de six (6) mois.</br>
+		</p>
+		<p>==>GESTION BASES DE DONNEES : L'apprenant sera initié aux outils bureautiques de Windows, Word et Excel + l'Internet sur une période complète de six (6) mois.
+		</p>
+		<p>==>GESTION PROJETS INFORMATIQUE : L'apprenant sera initié à la création et à la gestion des projets de conception informatique (création de logiciel, développement de programme, administration des services IT, conduite et supervision, etc.) sur une période complète de six (6) mois.
+		</p>
+	</div>
+	<div class="contact">Nous sommes situés au quartier sabon-gari (AGADEZ) à côté du bureau de l'OIM, pour d'autres
+		renseignements complémentaires, appelez nous aux : +227 89 32 55 52-EMAIL: groupeiirniger@gmail.com
+	</div>
+	</div>
+<span class="cont-img"></span>
+</body>
+</html>
